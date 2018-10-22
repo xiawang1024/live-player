5b0b4fd8a7872ed979d207394fc209ddebc959dd
wangxia/work <xiawang1024@icloud.com>
2018-10-22 11:13:55 +0800

    feat:slide,better-scroll

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Home msg="Welcome to Your Vue.js App" />
+    <Home />
   </div>
 </template>
 
 <script>
-import Home from './components/Home.vue'
+import Home from './components/Slide.vue'
 
 export default {
   name: 'app',
@@ -18,11 +17,12 @@ export default {
 
 <style lang="scss">
 #app {
+  width: 750px;
+  position: fixed;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,29 +1,28 @@
 <template>
-	<div class="hello">
-		<h1 @click="successTips">{{ msg }}</h1>
-		<simplert :useRadius="true" :useIcon="true" ref="simplert">
-		</simplert>
-		<live-player :src='src' :poster='poster'></live-player>
-	</div>
+  <div class="home">
+    <simplert :useRadius="true" :useIcon="true" ref="simplert">
+    </simplert>
+    <live-player :src='src' :poster='poster'></live-player>
+    <comment></comment>
+  </div>
 </template>
 
 <script>
 import Simplert from 'vue2-simplert'
 import LivePlayer from 'base/livePlayer/index'
+import Comment from 'base/comment/index'
 export default {
-  name: 'HelloWorld',
+  name: 'home',
   components:{
-	Simplert,
-	LivePlayer
+    Simplert,
+    LivePlayer,
+    Comment
   },
   data() {
 	  return {
 		  src:'http://ivi.bupt.edu.cn/hls/chchd.m3u8',
 		  poster:'https://a.weixin.hndt.com/h5/1066/live/livePost.jpg'
 	  }
-  },
-  props: {
-    msg: String
   },
   methods:{
     successTips() {
@@ -41,18 +40,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
